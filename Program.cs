be85992c246bd6c26d23402191dd1a7c57e8e832
McKay <daibinhua@gmail.com>
2017-10-10 22:29:38 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using NFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuronFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NeuronNetwork network = NeuronNetwork.Create(2, new int[] { 2,4,6 }, 1);
+            network.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
